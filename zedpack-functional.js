be16412371aa5fb6cf333e9ef71d64bc38335afb
Patrick Stockwell <stockwellpatrick@gmail.com>
2017-12-22 19:57:18 +1100

    urgh this is hard

--- zedpack-functional.js
+++ zedpack-functional.js
@@ -1,34 +1,41 @@
 #! /usr/bin/env node
 const S = require('sanctuary')
+const R = require('ramda')
 const argv = require('minimist')(process.argv.slice(2))
 const fs = require('fs')
 const Future = require('fluture')
 
+const nonMatcher = /$a/ // a RegExp that matches nothing.
 const getFileContents = file => (Future.node(done => { fs.readFile(file, 'utf8', done) }))
 const writeFile = contents => (fs.writeFile(argv.output, contents, writeFileSuccess))
-
-const noFileFound = x => {}
+const errorMessage = () => console.log('Sorry, looks like we ran into a problem.')
 const writeFileSuccess = err => err ? (
     console.log('Could not write to file.')
 ) : (
-    console.log('jolly good, all saved.')
+    console.log('Jolly good, all saved.')
 )
-
-const getCss = getFileContents(argv.css ? argv.css : '')
-const getJs = getFileContents(argv.js ? argv.js : '')
-const getHtml = getFileContents(argv.html ? argv.html : '')
-
 // A function which knows what kind of output is expected
 const createOutput = argv.output ? x => writeFile(x) : x => console.log(x)
 
+const getHtml = getFileContents(argv.html ? argv.html : '')
+// getHtml.fork(console.log, console.log)
+const splitOnNewline = R.lift(str => typeof str === 'String' ? str.split('\n') : null)
+const splitHtml = splitOnNewline(getHtml)
+
 // Wraps Futures in an 'Either' type
-const stabalizedFutures = [getCss, getJs, getHtml].map(Future.fold(S.Left, S.Right));
+// const stabalizedFuture = R.map(Future.fold(S.Left, S.Right))
+
+// console.log(stabalizedFuture(getCss))
 // Runs all Futures in parallel 
-Future.parallel(3, stabalizedFutures).fork(createOutput, createOutput);
+// Future.parallel(3, stabalizedFutures).fork(errorMessage, createOutput);
 
-// // // create output
+// console.log('here', R.map(splitHtml))
+splitHtml.fork(console.log, console.log)
 
-// const split
-// const findTag = filter(regex)
-// const replaceStyleTag
-// const replaceScriptTag
+
+const getCss = getFileContents(argv.css ? argv.css : '')
+const getJs = getFileContents(argv.js ? argv.js : '')
+const hasFileName = R.curry((regexObj, str) => regexObj.test(str))
+const hasCssFileName = hasFileName(new RegExp(argv.css))
+const checkForLine = R.curry((booleanFunc, replacement, line) => booleanFunc(line) ? replacement : line)
+const updateCssLine = checkForLine(hasCssFileName, R.map(getCss))
